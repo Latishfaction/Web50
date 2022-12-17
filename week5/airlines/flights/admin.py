fe263a87ca1e86dd2af4f1f2212ff1191f0b22f7
Latishfaction <latishchambhare@gmail.com>
2022-12-17 18:34:52 +0530

    added manytomany relationship

--- week5/airlines/flights/admin.py
+++ week5/airlines/flights/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Flight,Airport,Passengers
 
 # Register your models here.
+class flightAdmin(admin.ModelAdmin):
+    list_display = ('id','origin','destination','duration')
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ["flight"]
+
+admin.site.register(Flight,flightAdmin)
+admin.site.register(Airport)
+admin.site.register(Passengers,PassengerAdmin)
--- week5/airlines/flights/urls.py
+++ week5/airlines/flights/urls.py
@@ -1,8 +1,10 @@
 from . import views 
 from django.urls import path 
 
-app_name='flights'
+app_name='flights_data'
 
 urlpatterns=[
-    
+    path("",views.index,name="index"),
+    path("<int:flight_id>",views.flight,name="flight_details"),
+    path("<int:flight_id>/book",views.book,name="book"),
 ]

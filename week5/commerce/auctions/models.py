9d8221ad017bc2ae2297658d6c96d9608d567048
Latishfaction <latishchambhare@gmail.com>
2023-01-06 18:52:03 +0530

    reset the commerce project

--- week5/commerce/auctions/models.py
+++ week5/commerce/auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+# Auctions lists
+class listings(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    price = models.IntegerField()
+    url = models.URLField(max_length=400)
+    category = models.CharField(max_length=20)

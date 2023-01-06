57e2d2676862c5d2cb7c9d7fb82764732d85d7d8
Latishfaction <latishchambhare@gmail.com>
2023-01-06 19:27:55 +0530

    reset commerce configuration again

--- week5/commerce/auctions/admin.py
+++ week5/commerce/auctions/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
-from .models import listing
 # Register your models here.
-admin.site.register(listing)
--- week5/commerce/auctions/models.py
+++ week5/commerce/auctions/models.py
@@ -4,13 +4,3 @@ from django.db import models
 
 class User(AbstractUser):
     pass
-
-# Auctions lists
-class listing(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
-    title = models.CharField(max_length=30)
-    description = models.TextField(max_length=300)
-    price = models.PositiveIntegerField()
-    url = models.URLField(max_length=400)
-    category = models.CharField(max_length=20)
-

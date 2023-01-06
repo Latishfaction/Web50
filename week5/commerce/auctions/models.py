9277f73f54ab95b0d7a2faf6fff6d0b60fe50ac4
Latishfaction <latishchambhare@gmail.com>
2023-01-06 19:38:34 +0530

    made categry and auctions models

--- week5/commerce/auctions/models.py
+++ week5/commerce/auctions/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+# Auction Categories
+class Category(models.Model):
+    category = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.id} : {self.category}"
+
+# Auctions lists
+class Listing(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    price = models.PositiveIntegerField()
+    url = models.URLField(max_length=400)
+    theme = models.ForeignKey(Category, on_delete=models.CASCADE,related_name="theme")

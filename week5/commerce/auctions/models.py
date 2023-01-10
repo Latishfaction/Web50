1d509c5c75e9cc704fc114a58d6cfa4552c73069
Latishfaction <latishchambhare@gmail.com>
2023-01-10 21:16:25 +0530

    all models created

--- week5/commerce/auctions/models.py
+++ week5/commerce/auctions/models.py
@@ -12,17 +12,19 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.id} : {self.category}"
 
+
 # Auctions lists
 class Listing(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
     title = models.CharField(max_length=60)
     description = models.TextField(max_length=500)
-    price = models.PositiveIntegerField()
+    price = models.IntegerField()
     url = models.URLField(max_length=400)
     theme = models.ForeignKey(Category, on_delete=models.CASCADE,related_name="theme")
 
     def __str__(self):
         return f"{self.id} : {self.title} | {self.owner}"
+
 
 #comments
 class Comment(models.Model):
@@ -36,3 +38,23 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user} : {self.listing.title} from {self.listing.theme.category}"
+
+# bid
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE,related_name="bidder",null=True)
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE,related_name="bid_item")
+    bid_amount = models.PositiveIntegerField()
+    old_amount = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.bid_amount} | {self.bidder} | {self.item.title}"
+
+#live auctions
+class AuctionListing(models.Model):
+    isActive = models.BooleanField(default=False)
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE,related_name="active_items",null=True)
+
+# Watch list
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="user",null=True)
+    items = models.ManyToManyField(Listing)

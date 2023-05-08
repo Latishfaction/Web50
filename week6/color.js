73cb5c732648d7478222499b6b9d2b24e551a4f2
Latishfaction <latishchambhare@gmail.com>
2023-05-08 14:15:07 +0530

    #

--- week6/color.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    document.querySelector("#colors").onchange = function(){
-        document.querySelector('h1').style.color = this.value;
-    }
-});

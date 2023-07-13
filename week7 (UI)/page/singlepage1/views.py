fb86b20486f1299344b207f125556c74a9687453
Latishfaction <latishchambhare@gmail.com>
2023-07-13 22:12:52 +0530

    #

--- week7 (UI)/page/singlepage1/views.py
+++ week7 (UI)/page/singlepage1/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello");
+    return render(request, 'singlepage1/index.html')
+
+text=["Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.",
+"One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections."]
+
+def section(request,number):
+    number = int(number)
+    return HttpResponse(text[number-1])
